4988c887604abfa5e192b9bab42a87946f9ff139
ch0322 <hongc93@163.com>
2018-08-26 10:48:01 +0800

    boyi first

---
+++ boyi/博易/js/page.js
@@ -0,0 +1,10 @@
+$(".header .mobileMenuBtn").click(function () {
+  $(".header .mobileMenuBtn").toggleClass("active");
+  $(".header .header_menu").toggleClass("active");
+  $(".header .mobileMenuBtn_shad").toggleClass("active");
+});
+$(".header .mobileMenuBtn_shad").click(function () {
+  $(".header .mobileMenuBtn").toggleClass("active");
+  $(".header .header_menu").toggleClass("active");
+  $(".header .mobileMenuBtn_shad").toggleClass("active");
+});
