a658ed7b812352b94412afcc54c9a3f545bccbda
ch0322 <hongc93@163.com>
2018-08-28 21:36:10 +0800

    solution edit

--- boyi/sources/js/page.js
+++ boyi/sources/js/page.js
@@ -1,17 +1,30 @@
-$(".header .mobileMenuBtn").click(function () {
+$(".header .mobileMenuBtn").click(function() {
   $(".header .mobileMenuBtn").toggleClass("active");
   $(".header .header_menu").toggleClass("active");
   $(".header .mobileMenuBtn_shad").toggleClass("active");
 });
-$(".header .mobileMenuBtn_shad").click(function () {
+$(".header .mobileMenuBtn_shad").click(function() {
   $(".header .mobileMenuBtn").toggleClass("active");
   $(".header .header_menu").toggleClass("active");
   $(".header .mobileMenuBtn_shad").toggleClass("active");
 });
-$(window).scroll(function () {
+$(window).scroll(function() {
   if (jQuery(this).scrollTop() > 1) {
     $(".header").addClass("header_scroll");
   } else {
     $(".header").removeClass("header_scroll");
   }
 });
+
+//add class 解决方案2
+$(".solution-sec .name")
+  .mouseover(function() {
+    $(this)
+      .siblings(".shade")
+      .addClass("shadehover");
+  })
+  .mouseout(function() {
+    $(this)
+      .siblings(".shade")
+      .removeClass("shadehover");
+  });
